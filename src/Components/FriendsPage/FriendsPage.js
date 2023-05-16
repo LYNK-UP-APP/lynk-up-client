2e9e08651982babbbc6dce18783e240dad4c9115
trevorfitz0 <trevorfitz0@gmail.com>
2023-05-16 10:33:22 -0600

    General layout and search start created

--- src/Components/FriendsPage/FriendsPage.js
+++ src/Components/FriendsPage/FriendsPage.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './FriendsPage.css'
 
 function FriendsPage() {
+
+  const [friends, setFriends] = useState(['Trevor', 'Gus', 'Joe', 'Jill'])
+  const [search, setSearch] = useState('Search Friends');
+
+  const displayFriends = friends.map(friend => {
+    return <div className='long-tile'>{friend}</div>
+  })
+
+  const filterSearch = (value) => {
+    console.log(value)
+    setFriends(['Trevor', 'Gus', 'Joe', 'Jill'])
+    setFriends(friends.filter(friend => friend.includes(value)))
+  }
+
   return (
-    <div>FriendsPage</div>
+    <div>
+      <section className='card'>
+      <h1 className='title'>Friends</h1>
+        <input className='long-input'
+              value={search}
+              onChange={event => setSearch(event.target.value)}
+              onInput={event => filterSearch(event.target.value)}>
+        </input>
+            {
+              displayFriends
+            }
+      </section>
+    </div>
   )
 }
 
