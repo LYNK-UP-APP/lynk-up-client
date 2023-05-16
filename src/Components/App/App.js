391fdf7973d980dd3625025c61d039ff6a2e19ca
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-16 12:19:27 -0400

    Add: logo file
    Add: Header dropdown with various pages

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,18 +6,19 @@ import EventInfo from '../EventInfo/EventInfo';
 import FriendsPage from '../FriendsPage/FriendsPage';
 import GroupPage from '../GroupPage/GroupPage';
 import Header from '../Header/Header';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <CreateEvent></CreateEvent>
-        <Dashboard></Dashboard>
-        <ErrorPage></ErrorPage>
-        <EventInfo></EventInfo>
-        <FriendsPage></FriendsPage>
-        <GroupPage></GroupPage>
+        {/* <CreateEvent></CreateEvent> */}
+        <Route exact path="/dashboard" render={Dashboard} />
+        {/* <EventInfo></EventInfo> */}
+        <Route exact path="/friends" render={FriendsPage} />
+        <Route exact path="/groups" render={GroupPage} />
+        {/* <ErrorPage></ErrorPage> */}
       </main>
     </>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link, useHistory } from 'react-router-dom';
 import './Header.css';
+import logo from '../../assets/logo.png';
 
 function Header() {
+  const history = useHistory();
+
+  const handleChange = e => {
+    history.push(e.target.value);
+  }
 
   return (
     <header>
-      <h1>HEADER</h1>
+      <img className='header-logo' src={logo} />
+      <select className="nav-drop" onChange={handleChange}>
+        <option value="/groups"> Groups </option>
+        <option value="/friends"> Friends </option>
+        <option value="/dashboard"> Dashboard </option>
+      </select>
     </header>
   );
 }
