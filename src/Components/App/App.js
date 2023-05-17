f7bdd7d40f7c5ae9c157732c0bef15441fc5e7c2
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-05-17 12:40:08 -0400

    Fix app page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,21 +6,21 @@ import EventInfo from '../EventInfo/EventInfo';
 import FriendsPage from '../FriendsPage/FriendsPage';
 import GroupPage from '../GroupPage/GroupPage';
 import Header from '../Header/Header';
-import { Route, Switch} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <h1>YUH</h1>
       <Header></Header>
-
-      {/* <CreateEvent></CreateEvent> */}
+    
+      <CreateEvent></CreateEvent>
       <Route exact path="/" render={() => <Dashboard />} />
       <Route exact path="/groups" render={() => <GroupPage />} />
-      {/* <ErrorPage></ErrorPage>
+      <ErrorPage></ErrorPage>
       <EventInfo></EventInfo>
       <FriendsPage></FriendsPage>
-      <GroupPage></GroupPage> */}
+      <GroupPage></GroupPage>
     </div>
   );
 }
