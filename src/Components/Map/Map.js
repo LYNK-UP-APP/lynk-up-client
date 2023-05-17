ac7d4aba10206600e9f18abe5564718f0d7b2c70
trevorfitz0 <trevorfitz0@gmail.com>
2023-05-17 10:38:54 -0600

    Bug Fix

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,24 +1,24 @@
-import { useRef, useEffect } from "react";
-import "./Map.css";
+// import { useRef, useEffect } from "react";
+// import "./Map.css";
 
-const AutoComplete = () => {
- const autoCompleteRef = useRef();
- const inputRef = useRef();
- const options = {
-    fields: ["formatted_address", "geometry", "name"],
-    strictBounds: false,
-    types: ["establishment"]
-  };
- useEffect(() => {
-  autoCompleteRef.current = new window.google.maps.places.Autocomplete(
-   inputRef.current,
-   options
-  );
- }, []);
- return (
-  <div>
-   <input className="long-input" ref={inputRef}/>
-  </div>
- );
-};
-export default AutoComplete;
+// const AutoComplete = () => {
+//  const autoCompleteRef = useRef();
+//  const inputRef = useRef();
+//  const options = {
+//     fields: ["formatted_address", "geometry", "name"],
+//     strictBounds: false,
+//     types: ["establishment"]
+//   };
+//  useEffect(() => {
+//   autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+//    inputRef.current,
+//    options
+//   );
+//  }, []);
+//  return (
+//   <div>
+//    <input className="long-input" ref={inputRef}/>
+//   </div>
+//  );
+// };
+// export default AutoComplete;
