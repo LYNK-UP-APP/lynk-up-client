48d0ece09faa89f8c84567456f8c17b3ffc879ae
trevorfitz0 <trevorfitz0@gmail.com>
2023-05-17 10:51:19 -0600

    Minor comment error

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,9 +1,9 @@
-// import { useRef, useEffect } from "react";
-// import "./Map.css";
+import { useRef } from "react";
+import "./Map.css";
 
-// const AutoComplete = () => {
+const AutoComplete = () => {
 //  const autoCompleteRef = useRef();
-//  const inputRef = useRef();
+ const inputRef = useRef();
 //  const options = {
 //     fields: ["formatted_address", "geometry", "name"],
 //     strictBounds: false,
@@ -15,10 +15,10 @@
 //    options
 //   );
 //  }, []);
-//  return (
-//   <div>
-//    <input className="long-input" ref={inputRef}/>
-//   </div>
-//  );
-// };
-// export default AutoComplete;
+ return (
+  <div>
+   <input className="long-input" ref={inputRef}/>
+  </div>
+ );
+};
+export default AutoComplete;
