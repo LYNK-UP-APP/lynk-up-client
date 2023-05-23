6af4240559e4715f10a8dd89f95ed6412e3e2749
Trevor Fitzgerald <57536985+trevorfitz0@users.noreply.github.com>
2023-05-23 09:43:05 -0600

    Update GroupPage.js

--- src/Components/GroupPage/GroupPage.js
+++ src/Components/GroupPage/GroupPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './GroupPage.css';
 import { useSelector } from 'react-redux';
+import { getGroups } from '../../ApiCalls';
 // import { getFriends } from '../../ApiCalls';
 
 const dummyFriends = [
