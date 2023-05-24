acc2931864b9350464feea72ed51c1dfda2d1682
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-24 19:09:57 -0400

    Fix failing eventTile test

--- cypress/e2e/eventTile.cy.js
+++ cypress/e2e/eventTile.cy.js
@@ -1,15 +1,15 @@
 describe('Event Tile', () => {
     beforeEach(() => {
-      cy.intercept('GET', 'https://lynk-up-server.onrender.com/users/888-888-8888', {
-        fixture: 'getUser.json'
-    })
+      cy.login();
+      cy.getEventOne();
+      cy.clickEventOne();
       cy.intercept('GET', 'https://lynk-up-server.onrender.com/events/1', {
         fixture: 'event1.json'
       })
       cy.visit('http://localhost:3000/dashboard')
     });
   
-    it('Should tile that has name date and time', () => {
+    it('Should have a tile that has name date and time', () => {
       cy.get('.long-tile').first().should('contain', 'Magic Tournament').should('contain', '05/20/2023').should('contain', '5:00 PM MST');
     });
 
